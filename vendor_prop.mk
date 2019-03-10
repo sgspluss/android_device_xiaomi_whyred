f48bf1e050a9d5df745d54bbfebc02f39853ab1d
sgspluss <sgspluss94@gmail.com>
2019-03-10 12:31:55 +0100

    whyred: update vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,11 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.s5k3l8_ofilm.low.lux=379 \
     persist.s5k3l8_ofilm.light.lux=367 \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    vendor.vidc.enc.disable_bframes=1 \
-    vendor.vidc.dec.enable.downscalar=1 \
-    vendor.vidc.enc.disable.pq=false
+    persist.dbg.vt_avail_ovr=1
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -110,7 +106,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     persist.hwc.enable_vds=1 \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=480 \
+    ro.sf.lcd_density=420 \
     sdm.debug.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=0
 
@@ -130,20 +126,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.overlay.izat.optin=rro
-
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=88 \
-    ro.hwui.layer_cache_size=58 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=2048
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -169,6 +151,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable_bframes=1 \
     vendor.vidc.dec.enable.downscalar=1 \
     vendor.vidc.enc.disable.pq=false \
+    vidc.enc.dcvs.extra-buff-count=2 \
     video.disable.ubwc=1
 
 # Memory
@@ -222,7 +205,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.radio.schd.cache=3500 \
-    persist.sys.mcd_config_file=/system/etc/mcd_default.conf
+    persist.sys.mcd_config_file=/system/etc/mcd_default.conf \
     telephony.lteOnCdmaDevice=1 \
     ro.telephony.default_network=22,20 \
     ro.use_data_netmgrd=true \
@@ -231,13 +214,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
     persist.vendor.radio.snapshot_enabled=1 \
     persist.vendor.radio.snapshot_timer=5 \
-    persist.radio.sw_mbn_update=0
-    persist.radio.hw_mbn_update=0
+    persist.radio.sw_mbn_update=0 \
+    persist.radio.hw_mbn_update=0 \
     persist.radio.trigger.silence=true
 
 # Security Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2018-05-01
+    ro.vendor.build.security_patch=2019-03-01
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -260,7 +243,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.wfd.virtual=0
+    persist.sys.wfd.virtual=0 \
     persist.debug.wfd.enable=1
 
 # Wifi
